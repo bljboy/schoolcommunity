a4f3437cc0006094817d4749efa82f827d31e77d
bljboy <bljboy55@gmail.com>
2022-12-02 23:51:57 +0800

    tablayout 完成

--- app/src/main/java/com/bljboy/schoolcommunity/MainActivity.java
+++ app/src/main/java/com/bljboy/schoolcommunity/MainActivity.java
@@ -3,6 +3,7 @@ package com.bljboy.schoolcommunity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -12,6 +13,8 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import com.bljboy.schoolcommunity.nav_fragment.ChatFragment;
@@ -30,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private ToolsFragment toolsFragment;
     private MeFragment meFragment;
     private ImageButton drawerheader_image1;
+    private NavigationView drawerLayout;
+    private View drawer_header;
+    private ImageView drawer_header_image, drawer_header_image1,drawer_header_image3;
 
     @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
@@ -44,6 +50,21 @@ public class MainActivity extends AppCompatActivity {
         toolsFragment = new ToolsFragment();
         meFragment = new MeFragment();
         switchFragment(homeFragment);
+        //抽屉头部绑定
+        drawerLayout = findViewById(R.id.nav_left);
+        drawer_header = drawerLayout.inflateHeaderView(R.layout.drawer_header);
+        drawer_header_image = drawer_header.findViewById(R.id.drawer_header_image);
+        drawer_header_image1 = drawer_header.findViewById(R.id.drawer_header_image1);
+        drawer_header_image3 = drawer_header.findViewById(R.id.drawer_header_image3);
+        drawer_header_image3.setImageResource(R.drawable.one);
+        drawer_header_image1.getBackground().setAlpha(65);
+        drawer_header_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Log:", "头部被点击了！");
+            }
+        });
+
     }
 
     private NavigationBarView.OnItemSelectedListener my_navItem = new NavigationBarView.OnItemSelectedListener() {
@@ -51,19 +72,19 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.home:
+                case R.id.nav_home:
                     Log.d("Nav", "首页");
                     switchFragment(homeFragment);
                     break;
-                case R.id.chat:
+                case R.id.nav_chat:
                     switchFragment(chatFragment);
                     Log.d("Nav", "聊天");
                     break;
-                case R.id.tools:
+                case R.id.nav_tools:
                     switchFragment(toolsFragment);
                     Log.d("Nav", "工具");
                     break;
-                case R.id.me:
+                case R.id.nav_me:
                     switchFragment(meFragment);
                     Log.d("Nav", "我的");
                     break;
